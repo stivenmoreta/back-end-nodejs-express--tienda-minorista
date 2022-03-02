4267f964cc1b0249541d7d6797dddb2f2a617c27
enero70 <stivenmoreta.c@gmail.com>
2022-03-02 13:44:22 -0300

    eliminar producto mediante actualizacion de estado

--- src/controllers/productos.controller.js
+++ src/controllers/productos.controller.js
@@ -1,8 +1,14 @@
 const pool = require("../db");
 
-const getProductos = async (req, res) => {
+const getProductosDisponibles = async (req, res) => {
   const allProductos = await pool.query(
-    'SELECT id_producto, nombre_producto, precio_producto, stock_producto FROM "PRODUCTO"'
+    'SELECT id_producto, nombre_producto, precio_producto, stock_producto FROM "PRODUCTO" WHERE eliminado = false and stock_producto > 0'
+  );
+  res.json(allProductos.rows);
+};
+const getProductosRegistrados = async (req, res) => {
+  const allProductos = await pool.query(
+    'SELECT id_producto, nombre_producto, precio_producto, stock_producto, eliminado FROM "PRODUCTO" where eliminado = false'
   );
   res.json(allProductos.rows);
 };
@@ -16,7 +22,8 @@ const getProducto = async (req, res) => {
   res.json(producto.rows[0]);
 };
 const createProducto = async (req, res) => {
-  const { fk_id_categoria, nombre_producto, precio_producto, stock_producto } = req.body;
+  const { fk_id_categoria, nombre_producto, precio_producto, stock_producto } =
+    req.body;
   const newProducto = await pool.query(
     'INSERT INTO "PRODUCTO"(fk_id_categoria, nombre_producto, precio_producto, stock_producto) VALUES ($1,$2,$3,$4)',
     [fk_id_categoria, nombre_producto, precio_producto, stock_producto]
@@ -24,24 +31,56 @@ const createProducto = async (req, res) => {
   res.json(newProducto.rows);
 };
 const updateProducto = async (req, res) => {
-  const {fk_id_categoria,nombre_producto,precio_producto,stock_producto,id_producto} = req.body
+  const { id_producto } = req.params;
+  const { fk_id_categoria, nombre_producto, precio_producto, stock_producto } =
+    req.body;
   const allproductos = await pool.query(
-  'UPDATE "PRODUCTO" SET fk_id_categoria=$1, nombre_producto=$2, precio_producto=$3, stock_producto=$4 WHERE id_producto=$5',
-  [fk_id_categoria,nombre_producto,precio_producto,stock_producto,id_producto]
+    'UPDATE "PRODUCTO" SET fk_id_categoria=$1, nombre_producto=$2, precio_producto=$3, stock_producto=$4 WHERE id_producto=$5',
+    [
+      fk_id_categoria,
+      nombre_producto,
+      precio_producto,
+      stock_producto,
+      id_producto,
+    ]
   );
   res.json(allproductos.rows);
 };
 
 const deleteProducto = async (req, res) => {
-  /*agregar estado al producto, para borrarlo, solo cambia el estado*/
-  const allproductos = await pool.query("SELECT * FROM producto");
-  res.json(allproductos.rows);
+  try {
+    const { id_producto } = req.params;
+    const deleteProducto = await pool.query(
+      `UPDATE "PRODUCTO" SET eliminado = true
+    WHERE id_producto = $1 RETURNING id_producto,nombre_producto`,
+      [id_producto]
+    );
+    res.json(deleteProducto.rows[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const activateProducto = async (req, res) => {
+  try {
+    const { id_producto } = req.params;
+    const deleteProducto = await pool.query(
+      `UPDATE "PRODUCTO" SET eliminado = false
+    WHERE id_producto = $1 RETURNING id_producto,nombre_producto`,
+      [id_producto]
+    );
+    res.json(deleteProducto.rows[0]);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
-  getProductos,
+  getProductosDisponibles,
+  getProductosRegistrados,
   getProducto,
   createProducto,
   updateProducto,
   deleteProducto,
+  activateProducto
 };
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,19 +1,23 @@
 const { Router } = require("express");
 const {
-  getProductos,
+  getProductosDisponibles,
+  getProductosRegistrados,
   getProducto,
   createProducto,
   updateProducto,
   deleteProducto,
+  activateProducto
 } = require("../controllers/productos.controller");
-const { verifyToken, isGestor } = require("../middlewares/auth.jwt");
+const { verifyToken, isGestor, isAdmin } = require("../middlewares/auth.jwt");
 
 const router = Router();
 
-router.get("/", verifyToken, getProductos); //listo
+router.get("/", verifyToken, getProductosDisponibles);
+router.get("/registrados", [verifyToken,isGestor], getProductosRegistrados); //listo
 router.get("/:id_producto", verifyToken, getProducto); //listo
 router.post("/", [verifyToken, isGestor], createProducto); //listo
-router.put(":id_producto", [verifyToken, isGestor], updateProducto); //listo
-router.delete(":id_producto", [verifyToken, isGestor], deleteProducto);
+router.put("/:id_producto", [verifyToken, isGestor], updateProducto); //listo
+router.delete("/:id_producto", [verifyToken,isGestor],deleteProducto);
+router.put("/activate/:id_producto", [verifyToken,isGestor],activateProducto);
 
 module.exports = router;
